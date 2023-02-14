3f25a655310323b72ff0801f2118a0708fa49a49
kamal heydari <kamal3heydari@gmail.com>
2023-02-14 13:23:41 +0330

    chapter - 2 setup project

---
+++ store/cartSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartslice = createSlice({
+  name: "cart",
+  initialState: [],
+  reducers: {},
+});
+
+// export const {}=cartslice.actions;
+
+export default cartslice.reducer;
